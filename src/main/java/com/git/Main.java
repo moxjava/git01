21b8085e9429049366649dc103f4fc9f74dd4de1
moxjava <145752416+moxjava@users.noreply.github.com>
2023-09-24 16:06:30 +0800

    Update Main.java
    pull test

--- src/main/java/com/git/Main.java
+++ src/main/java/com/git/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
